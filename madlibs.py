6f04c31ef09d7fec5fb822add986a910daa2ae6d
Ezinne Anne Emilia <onwukaezinneanne@gmail.com>
2022-02-09 10:45:49 +0100

    This is a simple python game
    I wrote this madlibs game to test my python knowledge

---
+++ madlibs.py
@@ -0,0 +1,35 @@
+#My personal projects
+#No 1- Madlibs Game
+
+# I will try to create a story with Madlibs.
+
+noun_name = input("Enter the name of the protagonist: ")
+
+noun_sales = input("Enter the word 'sales': ")
+
+noun_manager = input("Enter the word 'manager': ")
+
+noun_status = input("Enter the word 'staff': ")
+
+noun_state = input("Enter the word 'jobs': ")
+
+noun_relation = input("Enter the word 'colleagues': ")
+
+noun_order = input("Enter the word 'management': ")
+
+first_conjunction = input("Enter the word 'and': ")
+
+second_conjunction = input("Enter the word 'with': ")
+
+first_pronoun = input("Enter the word 'her': ")
+
+second_pronoun = input("Enter the word 'she': ")
+
+third_pronoun = input("Enter the word 'their': ")
+
+preposition = input("Enter the word 'to': ")
+
+
+madlib = f"{noun_name}, a {noun_sales} representative at Lynst telecoms company has been working there for two years now. {second_pronoun} {first_conjunction} {first_pronoun} {noun_relation} would always complain about the {noun_sales} {noun_manager}. {first_pronoun} {noun_sales} {noun_manager} was always mean {first_conjunction} would easily report any annoying {noun_status} {second_pronoun} did not like {preposition} {noun_order} {first_conjunction} that {noun_status} would be sacked. It made {noun_name} {first_conjunction} {first_pronoun} {noun_relation} depressed about {third_pronoun} {noun_state}. They were always afraid of failing {preposition} obey the instructions of {third_pronoun} {noun_sales} {noun_manager} as it could cost them {third_pronoun} {noun_state}. Even {second_conjunction} {noun_name}'s precautions, twice {second_pronoun} had been reported {preposition} the {noun_order} by {first_pronoun} {noun_sales} {noun_manager} {first_conjunction} {second_pronoun} was warned severely. A year ago, {noun_name} heard about the feedback staircase {first_conjunction} resolved {preposition} apply it in {first_pronoun} communication {second_conjunction} {first_pronoun} {noun_sales} {noun_manager}. It turned out the {noun_sales} {noun_manager} was not always able {preposition} communicate effectively {second_conjunction} {first_pronoun} team."
+
+print(madlib)
