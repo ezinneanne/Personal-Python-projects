46034fb3e604d45bf34ee2ebd0b09edc38819a01
Ezinne Anne Emilia <onwukaezinneanne@gmail.com>
2022-02-17 19:33:48 +0100

    Adding files

---
+++ linear search algorithm.py
@@ -0,0 +1,27 @@
+def linear_search(list,target):
+    
+    for i in range(0,len(list)):
+        if list[i] == target:
+            return i
+    return None
+    
+    
+
+def verify(index):
+    if index is not None:
+        print("Target found at index: ",index)
+    else:
+        print("Target not found in list")
+        
+        
+        
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+
+result = linear_search(numbers,10)
+verify(result)
+
+
+
+
+    
